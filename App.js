58accfa237340081e0e05a175999c99614ac7200
TimothyJounier <timothyjounier@gmail.com>
2024-01-16 15:38:46 +0100

    create card and modification IF

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { View } from "react-native";
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
+import { CardTodo } from "./components/CardTodo/CardTodo";
+
+const TODO_LIST = [
+  { id: 1, title: "Sortir le chien", isCompleted: true },
+  { id: 2, title: "Faire un bisou à crapautin", isCompleted: false },
+];
 
 export default function App() {
   return (
@@ -11,7 +17,9 @@ export default function App() {
           <View style={s.header}>
             <Header></Header>
           </View>
-          <View style={s.body}></View>
+          <View style={s.body}>
+            <CardTodo todo={TODO_LIST[0]}></CardTodo>
+          </View>
         </SafeAreaView>
       </SafeAreaProvider>
       <View style={s.footer}></View>
---
+++ components/CardTodo/CardTodo.jsx
@@ -0,0 +1,10 @@
+import { TouchableOpacity, Text, Image} from "react-native";
+import checkImg from "../../assets/check.png";
+import {s} from "./CardTodo.style";
+
+export function CardTodo({ todo} ){
+    return <TouchableOpacity style={s.card}>
+        <Text style={[s.text,todo.isCompleted && {textDecorationLine:"line-through"}]}>{todo.title}</Text>
+        {todo.isCompleted && <Image style={s.img} source={checkImg}></Image>}
+    </TouchableOpacity>;
+}
