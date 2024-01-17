4409420773e6198bd7542661bc84eca12384462b
TimothyJounier <timothyjounier@gmail.com>
2024-01-17 15:57:43 +0100

    Add btn todo

--- App.js
+++ App.js
@@ -5,22 +5,19 @@ import { Header } from "./components/Header/Header";
 import { CardTodo } from "./components/CardTodo/CardTodo";
 import { useState } from "react";
 import { TabBottomMenu } from "./components/TabBottomMenu/TabBottomMenu";
+import { ButtonAdd } from "./components/ButtonAdd/ButtonAdd";
+import Dialog from "react-native-dialog";
+import uuid from "react-native-uuid";
 
 export default function App() {
   const [selectedTabName, setSelectedTabName] = useState("all");
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "Sortir le chien", isCompleted: true },
-    { id: 2, title: "Faire un bisou à crapautin", isCompleted: false },
-    { id: 3, title: "Faire les courses", isCompleted: false },
-    { id: 4, title: "Appeler le vétérinaire", isCompleted: true },
-    { id: 5, title: "Faire les courses", isCompleted: false },
-    { id: 6, title: "Appeler le vétérinaire", isCompleted: true },
-    { id: 7, title: "Faire les courses", isCompleted: false },
-    { id: 8, title: "Appeler le vétérinaire", isCompleted: true },
-  ]);
+  const [todoList, setTodoList] = useState([]);
+  const [isAddDialogVisible, setIsAddDialogVisible] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   /** FUNCTION */
 
+  // filter list
   function getFilterList() {
     switch (selectedTabName) {
       case "all":
@@ -31,6 +28,9 @@ export default function App() {
         todoList.filter((todo) => todo.isCompleted);
     }
   }
+
+  // add toDo
+  function addTodo() {}
 
   // update the toDo
   function updateTodo(todo) {
@@ -46,6 +46,7 @@ export default function App() {
     setTodoList(updatedTodoList);
   }
 
+  // delete to do
   function deleteTodo(todoToDelete) {
     Alert.alert("Suppression", "Supprimer cette tâche?", [
       {
@@ -75,6 +76,20 @@ export default function App() {
     ));
   }
 
+  function showAddDialog() {
+    setIsAddDialogVisible(true);
+  }
+
+  function addTodo() {
+    const newTodo = {
+      id: uuid.v4(),
+      title: inputValue,
+      isCompleted: false,
+    };
+    setTodoList([...todoList, newTodo]);
+    setIsAddDialogVisible(false);
+  }
+
   return (
     <>
       <SafeAreaProvider>
@@ -85,6 +100,7 @@ export default function App() {
           <View style={s.body}>
             <ScrollView>{renderTodoList()}</ScrollView>
           </View>
+          <ButtonAdd onPress={showAddDialog}></ButtonAdd>
         </SafeAreaView>
       </SafeAreaProvider>
       <View style={s.footer}>
@@ -94,6 +110,21 @@ export default function App() {
           selectedTabName={selectedTabName}
         ></TabBottomMenu>
       </View>
+      <Dialog.Container
+        visible={isAddDialogVisible}
+        onBackdropPress={() => setIsAddDialogVisible(false)}
+      >
+        <Dialog.Title>Créer une tâche</Dialog.Title>
+        <Dialog.Description>
+          Choisi un nom pour la nouvelle tâche
+        </Dialog.Description>
+        <Dialog.Input onChangeText={setInputValue}></Dialog.Input>
+        <Dialog.Button
+          disabled={inputValue.trim().length === 0}
+          label="Créer"
+          onPress={addTodo}
+        ></Dialog.Button>
+      </Dialog.Container>
     </>
   );
 }
